c8902eb7e948fc5dbc546f4a42b010865cc92648
Lily <117360193+LilyKhakimova@users.noreply.github.com>
2023-05-17 20:05:29 +0300

    app.py

--- app.py
+++ app.py
@@ -1,49 +1,49 @@
 import telebot
-from config import token, keys
-from extensions import ExchangeException, Exchange
+from config import TOKEN, values, menu_list, help_list
+from extensions import APIException, CryptoConverter
 
-bot = telebot.TeleBot(token)
+
+bot = telebot.TeleBot(TOKEN)
+
+
+@bot.message_handler(commands=['menu'])
+def main_menu(message):
+    bot.send_message(message.chat.id, menu_list)
 
 
 @bot.message_handler(commands=['start'])
-def start(message: telebot.types.Message):
-    text = 'Привет! Я Бот-Конвертер валют и я могу:  \n- Показать список доступных валют через команду /values \
-    \n- Вывести конвертацию валюты через команду <имя валюты> <в какую валюту перевести> <количество переводимой валюты>\n \
-- Напомнить, что я могу через команду /help'
-    bot.reply_to(message, text)
+def start_message(message):
+    bot.send_message(message.chat.id, "Здравствуйте! Этот бот поможет Вам с конвертацией валют. Для просмотра доступных команд введите ‘/menu’")
 
 
 @bot.message_handler(commands=['help'])
-def help(message: telebot.types.Message):
-    text = 'Чтобы начать конвертацию, введите команду боту в следующем формате: \n<имя валюты> <в какую валюту перевести> <количество переводимой валюты>\nЧтобы увидеть список всех доступных валют, введите команду\n/values'
-    bot.reply_to(message, text)
+def help_message(message):
+    bot.send_message(message.chat.id, help_list)
 
 
 @bot.message_handler(commands=['values'])
-def values(message: telebot.types.Message):
-    text = 'Доступные валюты:'
-    for key in keys.keys():
-        text = '\n'.join((text, key,))
-    bot.reply_to(message, text)
+def values_message(message):
+    bot.send_message(message.chat.id, 'Доступные валюты:')
+    for i in values:
+        bot.send_message(message.chat.id, i + ' ' + values[i])
 
 
 @bot.message_handler(content_types=['text'])
-def get_price(message: telebot.types.Message):
+def convert_result(message: telebot.types.Message):
     try:
-        values = message.text.split(' ')
+        val = message.text.split(' ')
 
-        if len(values) != 3:
-            raise ExchangeException('Введите команду или 3 параметра')
+        if len(val) != 3:
+            raise APIException('Неверно введены данные')
 
-        quote, base, amount = values
-        total_base = Exchange.get_price(quote, base, amount)
-    except ExchangeException as e:
-        bot.reply_to(message, f'Ошибка пользователя.\n{e}')
+        base, quote, amount = val
+        result = CryptoConverter.convert(base, quote, amount)
+    except APIException as e:
+        bot.reply_to(message, f'Ошибка пользователя.\n {e}')
     except Exception as e:
-        bot.reply_to(message, f'Что-то пошло не так с {e}')
+        bot.reply_to(message, f'Не удалось обработать команду.\n {e}')
     else:
-        text = f'Переводим {quote} в {base}\n{amount} {quote} = {total_base} {base}'
+        text = f'{amount} {values[base]} в {values[quote]} равно: {result}'
         bot.send_message(message.chat.id, text)
 
-
-bot.polling()
+bot.polling(none_stop=True)
